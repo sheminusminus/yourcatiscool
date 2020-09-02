56eee58333e1b32507128bb22f89b70996f9b28e
Emily Kolar <emkolar@gmail.com>
2020-09-01 22:52:36 -0700

    add kitty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import cat from './cat.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cat} className="App-logo" alt="cat" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          your cat is the coolest
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
